2beb4d361cae3bc9daf20543adb4a16ebdb5fc49
taka <taka@users.noreply.github.com>
2024-09-20 17:08:26 +0900

    詳細ページにある外部リンクをカスタムフィールドから取得するようにした。リンクが設定されていない場合、リンクボタンを表示しないようにした。メインの画像にもリンクを設定。

--- single.php
+++ single.php
@@ -8,11 +8,22 @@
             <div id="post-<?php the_ID(); ?>" <?php post_class('p-worksDetail'); ?>>
                 <h2 class="p-worksDetail__heading"><?php the_title(); ?></h2>
                 <div class="p-worksDetail__overViewCtn">
-                    <div class="p-worksDetail__overViewImg">
-                        <?php the_post_thumbnail('full'); ?>
-                    </div>
+                    <?php
+                    $url = get_field('site_url');
+                    $hrefAttr = '';
+                    if ($url != '') :
+                        $hrefAttr = 'href="' . esc_url($url) . '"';
+                    ?>
+                        <div class="p-worksDetail__overViewImg">
+                            <a <?php echo $hrefAttr; ?> target="_blank" rel="noopener noreferrer"><?php the_post_thumbnail('full'); ?></a>
+                        </div>
 
-                    <a class="p-worksDetail__SiteLink"><span class="p-worksDetail__SiteLink__caption">サイトを見る</span></a>
+                        <a <?php echo $hrefAttr; ?> target="_blank" rel="noopener noreferrer" class="p-worksDetail__SiteLink"><span class="p-worksDetail__SiteLink__caption">サイトを見る</span></a>
+                    <?php else: ?>
+                        <div class="p-worksDetail__overViewImg">
+                            <?php the_post_thumbnail('full'); ?>
+                        </div>
+                    <?php endif; ?>
                 </div>
 
                 <?php the_content(); ?>
