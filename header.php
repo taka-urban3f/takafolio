6860ba92a2a062e37b22e92089a3a11103b6d5ba
taka <taka@users.noreply.github.com>
2024-09-27 12:52:25 +0900

    ogp追加

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
-<head>
+<head prefix="og: https://ogp.me/ns#">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-RFL87FLY8N"></script>
     <script>
@@ -14,11 +14,18 @@
 
         gtag('config', 'G-RFL87FLY8N');
     </script>
-    
+
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex, nofollow">
     <meta name="description" content="<?php bloginfo('description'); ?>">
+
+    <meta property="og:url" content="<?php echo esc_url(site_url()); ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="<?php bloginfo('name'); ?>" />
+    <meta property="og:description" content="<?php bloginfo('description'); ?>" />
+    <meta property="og:site_name" content="<?php bloginfo('name'); ?>" />
+    <meta property="og:image" content="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/ogp_image.png">
 
     <link rel="icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon.ico">
     <link rel="apple-touch-icon" type="image/png" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/apple-touch-icon-180x180.png">
