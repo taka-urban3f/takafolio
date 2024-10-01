3741d4fc397a8451da2ca8168a6f76bb769f1d3e
taka <taka@users.noreply.github.com>
2024-10-01 16:17:39 +0900

    ４０４ページ作成。

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<main>
+    <div class="p-notFound">
+        <p>お探しのページは、移動もしくは削除された可能性があります。</p>
+        <a href="<?php echo esc_url(home_url()); ?> "  class="p-notFound__link">トップページはこちらです。</a>
+    </div>
+</main>
+</div>
+<!-- .l-wrapperここまで -->
+
+<?php get_footer(); ?>
